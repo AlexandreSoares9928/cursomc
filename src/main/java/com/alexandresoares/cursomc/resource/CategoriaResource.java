703c877bc5cef1ed48c20e9e95b1079070b3e028
AlexandreSoares9928 <alexandresoares9928@gmail.com>
2023-11-15 01:08:41 -0300

    Testando classe de dominio - CAtegoria

--- src/main/java/com/alexandresoares/cursomc/resource/CategoriaResource.java
+++ src/main/java/com/alexandresoares/cursomc/resource/CategoriaResource.java
@@ -1,16 +1,30 @@
 package com.alexandresoares.cursomc.resource;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.alexandresoares.cursomc.domain.Categoria;
+
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
-	
+
 	@GetMapping
-	public String listar() {
-		return "<h1>REST funcionando!</h1>";
+	public List<Categoria> listar() {
+
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+
+		return lista;
+
 	}
 
 }
